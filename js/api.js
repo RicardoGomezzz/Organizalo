c1272094ea12f47dc12e1353ad84488a9c92f1c7
Ricardo <ric.gomezp@duocuc.cl>
2022-05-23 13:37:30 -0400

    cambios y test

---
+++ js/api.js
@@ -0,0 +1,20 @@
+const url ="https://pokeapi.co/api/v2/pokemon/3"
+
+fetch(url)
+.then(response => response.json())
+.then(data => {
+
+    let element =document.getElementById("elem")
+    element.innerHTML = `
+
+    <p>${data.name}</p>
+    <p>${data.order}</p>
+    <img src="${data.sprites.font_default}"/>
+    
+    
+    `;
+    console.log(data)
+
+
+})
+.catch(err=>console.log(err))
--- js/validacionform.js
+++ js/validacionform.js
@@ -30,6 +30,7 @@ $("#formlogin").validate({
     }
   });
 
+  
 
 
 
