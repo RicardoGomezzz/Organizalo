44176e4375f3881d1585eb549f9c48e9bc2d4644
Felipe <feli.hidalgom@duocuc.cl>
2022-06-27 17:29:07 -0400

    avanze

--- organizalo/models.py
+++ organizalo/models.py
@@ -4,5 +4,21 @@ from django.db import models
 # Create your models here.
 
 
-class Descripcion{models.model}:
-    Descripcionn = models.charfield{max_length=300}
+class Descripcion(models.model):
+    Descripcionn = models.CharField(max_length=300)
+    Descripcionn = models.ForeignKey(Monto, on_delete=models.DO_NOTHING)
+    
+    def __str__(self):
+        return self.Descripcionn
+    
+class Monto(models.Model):
+    Cantidad = models.IntegerField(max_length=10)
+
+class Cuenta(models.Model):
+    Tipo= models.CharField(max_length=50)
+
+    
+    
+
+
+    
