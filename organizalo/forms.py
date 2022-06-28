5c5d3eaca2ee3f3b9acfd2db194b86dbfbf8a8bf
Felipe <feli.hidalgom@duocuc.cl>
2022-06-28 15:39:09 -0400

    a

--- organizalo/forms.py
+++ organizalo/forms.py
@@ -9,10 +9,5 @@ from organizalo.models import Transaccion
 
 class FormularioTransaccion (forms.ModelForm):
     class Meta:
-<<<<<<< HEAD
-        model = Transaccion
-=======
         models = Transaccion
->>>>>>> e9f7098e78f2df6f9ed95f1b15770ee570354f90
         fields = '__all__'
-        widgets = ()
