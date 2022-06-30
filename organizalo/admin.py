16e1afaa1d5467bd135a9f1de44dd8c3a5070f3b
Felipe <feli.hidalgom@duocuc.cl>
2022-06-30 15:40:26 -0400

    a

--- organizalo/admin.py
+++ organizalo/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import Transaccion
 # Register your models here.
 
+
+
+
 class TransaccionAdmin(admin.ModelAdmin):
     list = ('Descripcion','Monto','Cuenta','codigo')
     admin.site.register(Transaccion)
