a24c0dabb225165dc90f55bf1161be3e3671c0ed
Felipe <feli.hidalgom@duocuc.cl>
2022-06-30 22:02:59 -0400

    a

--- organizaloo/views.py
+++ organizaloo/views.py
@@ -1,8 +1,6 @@
-import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def index(request):  #PAGINA 1
-    return render(request, 'organizalo/base.html')
-
+def bievenida(request):
+    return render (HttpResponse("Bienvenidos a la pagina web"))
