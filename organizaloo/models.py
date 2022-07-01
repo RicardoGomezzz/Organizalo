d681c66dc3343d551cf6764cc14a7143bbb5ea3c
Felipe <feli.hidalgom@duocuc.cl>
2022-06-30 23:05:11 -0400

    falta pokito jejeje

--- organizaloo/models.py
+++ organizaloo/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+opciones_cuenta = [
+    
+    [0, 'Debito'],
+    [1, 'Visa'],
+    [2, 'Mach'],
+    [3, 'Efectivo']
+    
+]
+
+
+
+class Transaccion(models.Model):
+    Fecha = models.DateField()
+    Descripcion = models.CharField(max_length=150)
+    Monto = models.IntegerField()
+    Cuenta = models.IntegerField(choices=opciones_cuenta)
+    
+    def __str__(self):
+        return self.Monto
+    
