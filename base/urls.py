5291336921ff048ebedb356f51acfd4ddeba9e86
Felipe <feli.hidalgom@duocuc.cl>
2022-06-30 22:26:20 -0400

    a

--- base/urls.py
+++ base/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from rest_framework import routers
-from organizaloo import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('organizaloo.urls')),
-
 ]
