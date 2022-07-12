f621645eba5f448ded8efe9b408701427d0af0ac
Felipe <feli.hidalgom@duocuc.cl>
2022-07-12 13:34:59 -0400

    ashutate

--- organizaloo/forms.py
+++ organizaloo/forms.py
@@ -1,9 +1,14 @@
 from django import forms
-from .models import Transaccion
+from .models import Transaccion, Contacto
 
 class TransaccionForm(forms.ModelForm):
     
     
     class Meta:
         model = Transaccion
+        fields = '__all__'
+        
+class ContactoForm(forms.ModelForm):
+    class Meta:
+        model = Contacto
         fields = '__all__'
